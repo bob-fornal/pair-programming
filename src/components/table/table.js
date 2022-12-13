458fd2edc5eaa2f77b677057bd3283b12eb5f3c6
Bob Fornal <bob.fornal@leadingedje.com>
2022-12-13 08:49:08 -0500

    Base Changes

--- src/components/table/table.js
+++ src/components/table/table.js
@@ -24,10 +24,10 @@ function Table() {
   return (
     <table className="table">
       <thead>
-        <TitleRow headers={ data.headers } />
+        <TitleRow headers={ data.structure } />
       </thead>
       <tbody>
-        { data.content.map((row, index) => <TableRow key={ index } row={ row } />) }
+        { data.content.map((row, index) => <TableRow key={ index } index={ index } row={ row } structure={ data.structure } />) }
       </tbody>
     </table>
   );
@@ -38,20 +38,46 @@ function TitleRow(props) {
 
   return (
     <tr>
-      { headers.map((header, index) => <td key={ index }>{ header }</td>) }
+      { headers.map((header, index) => <td key={ index }>{ header.title }</td>) }
     </tr>
   )
 }
 
 function TableRow(props) {
   const row = props.row;
-  const keys = Object.keys(row);
+  const structure = props.structure;
+  const index = props.index;
+  console.log(index, index%2);
 
   return (
-    <tr>
-      { keys.map((key, index) => <td key={ index }>{ row[key] }</td>) }
+    <tr className={ index%2 === 0 ? 'odd' : 'even' }>
+      { structure.map((struct, index) => 
+          <TableData key={ index } index={ index } data={ row[struct.key] } struct={ struct } />
+      ) }
     </tr>
   );
+}
+
+function TableData(props) {
+  const index = props.index;
+  const data = props.data;
+  const struct = props.struct;
+
+  switch (true) {
+    case (struct.type === 'string'):
+      return <td key={ index }>{ data }</td>;
+    case (struct.type === 'date'):
+      const dataDate = new Date(data);
+      const dateFormat = { year: "numeric", month: "short", day: "numeric" };
+      const display = dataDate.toLocaleDateString('en-us', dateFormat);
+      return <td key={ index }>{ display }</td>;
+    case (struct.type === 'email'):
+      return <td key={ index }>
+        <a href={ `email:${ data }` }>{ data }</a>
+      </td>
+    default:
+      return <td key={ index }>{ data }</td>;
+  }
 }
 
 export default Table;
